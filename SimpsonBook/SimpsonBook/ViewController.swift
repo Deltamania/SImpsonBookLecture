4b3565c97696eed19f1010f02e6f5c371eb6eabb
Murat Han Acıpayam <acipayammurathan@gmail.com>
2019-10-15 15:00:59 +0300

    Add files via upload

---
+++ SimpsonBook/SimpsonBook/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by Murat Han on 15.10.2019.
+//  Copyright © 2019 Murat Han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SimpsonBook/SimpsonBook/detailVC.swift
@@ -0,0 +1,24 @@
+//
+//  detailVC.swift
+//  SimpsonBook
+//
+//  Created by Murat Han on 15.10.2019.
+//  Copyright © 2019 Murat Han. All rights reserved.
+//
+
+import UIKit
+
+class detailVC: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
